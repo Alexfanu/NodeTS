6e13722227854f6b1507a0065759b436986bd255
Alexfanu <78355480+Alexfanu@users.noreply.github.com>
2022-06-12 22:38:31 +0300

    Added express basic server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 
+import express, {Request,Response,Application} from 'express';
+const app:Application = express();
 
 
-function sum (num1:number, num2:number){
-    return num1 + num2;
-}
-console.log(sum(8,4))
+const PORT = process.env.PORT || 8000;
+
+
+
+
+app.get("/", (req:Request, res:Response):void => {
+    res.send("Hello Typescript with Node.js!")
+  });
+
+app.listen(PORT, ():void => {
+console.log(`Server Running here 👉 https://localhost:${PORT}`);
+});
+
+
+
